5158699014ea4ac2b5455781628457e02f63574b
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2025-08-26 19:27:46 +0100

    Add battle logic and button handlers to battle screen
    Implemented player and enemy choice logic for the battle system, including win/loss/draw conditions and cookie score updates. Connected new button signals for 'claw' and 'roar' actions, and added corresponding handler methods.

--- scripts/battle_screen.gd
+++ scripts/battle_screen.gd
@@ -5,11 +5,45 @@ extends Node2D
 @onready var roar_button := $CanvasLayer/roat_button
 @onready var cookie_counter := $CanvasLayer/cookie_counter
 
+var enemy_choice := ""
+var player_choice := ""
+var choices := ["paw", "claw", "roar"]
+
 func _ready() -> void:
 	update_score()
 
 func update_score() -> void:
 	cookie_counter.text = str(Global.total_cookies)
 
+func enemy_turn() -> String:
+	enemy_choice = choices[randi() % choices.size()]
+	return enemy_choice
+
+func battle() -> void:
+	enemy_turn()
+	print("enemy_chooses: ", enemy_choice)
+	
+	if player_choice == enemy_choice:
+		print("draw")
+	elif (player_choice == "roar" and enemy_choice == "paw" 
+	or player_choice == "paw" and enemy_choice == "claw" 
+	or player_choice == "claw" and enemy_choice == "roar"):
+		print("player win")
+		Global.total_cookies += 1
+	else:
+		print("player lose")
+		Global.total_cookies -= 1
+	
+	update_score()
+
 func _on_paw_button_pressed():
-	pass
+	player_choice = "paw"
+	battle()
+
+func _on_claw_button_pressed():
+	player_choice = "claw"
+	battle()
+
+func _on_roar_button_pressed():
+	player_choice = "roar"
+	battle()
